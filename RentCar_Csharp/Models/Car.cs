0db65f3e6a9293247dca470ba2a6c323b6a8af36
Lihao WEI <75092025+EBFantasy@users.noreply.github.com>
2023-01-03 11:35:05 +0100

    Add files via upload

---
+++ RentCar_Csharp/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace RentCar_Csharp.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public string Mark { get; set; }
+
+        public double Price { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
