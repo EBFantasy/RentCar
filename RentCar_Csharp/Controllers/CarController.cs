456ba537d2036305ff7fd550cab925be0f999363
Lihao WEI <75092025+EBFantasy@users.noreply.github.com>
2023-01-21 15:49:22 +0100

    Add files via upload
    connect to docker postgresql

--- RentCar_Csharp/Controllers/CarController.cs
+++ RentCar_Csharp/Controllers/CarController.cs
@@ -1,3 +1,41 @@
+<<<<<<< HEAD
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentCar_Csharp.Models;
+
+namespace RentCar_Csharp.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Car> GetCars()
+        {
+            return new Car[] { new Car()
+                {
+                    carId = 1,
+                    Mark = "Benz",
+                    Price = 180000.00,
+                    Stock = 5
+                },new Car()
+                {
+                    carId = 2,
+                    Mark = "BWM",
+                    Price = 145000.00,
+                    Stock = 18
+                }
+            };
+        }
+
+        [HttpDelete]
+        public string DeleteCars()
+        {
+            return "Delete successful";
+        }
+    }
+}
+=======
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RentCar_Csharp.Models;
@@ -40,3 +78,4 @@ namespace RentCar_Csharp.Controllers
         }
     }
 }
+>>>>>>> e23b2a445ed7ae23e0481e5879aacb0798a73827
